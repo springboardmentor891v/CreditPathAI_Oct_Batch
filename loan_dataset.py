b4d9ae6081741cbc434ce6f6b83adce316ced465
unknown <aditi.new1234@gmail.com>
2025-10-22 18:26:44 +0530

    changes

--- loan_dataset.py
+++ loan_dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd
-df= pd.read_csv("C:/Users/Admin/CreditPathAI_Oct_Batch/Loan_Default.csv")
+df= pd.read_csv("C:/Users/ADITI/CreditPathAI_Oct_Batch/datasets/Loan_Default.csv")
 print(df.head())
 print(df.describe())
 print(df.tail())
-print(df.info())
-
+print(df.info())     
