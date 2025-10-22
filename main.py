89e9f5ff82266a4bc27be038bf20b5cf52dce8ea
Purvi Porwal <purviporwal46@gmail.com>
2025-10-22 16:45:22 +0530

    Loaded Dataset and added DataFrames

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv('Loan_default.csv')
+
+print(df.head())
+print(df.tail())
+print(df.describe())
+print(df.info())
