10d868b3b1ba1cc026cad4cf58c6d11c274d041e
Aditi-Bibave <aditi.new1234@gmail.com>
2025-11-28 11:33:19 +0000

    demo streamlit

---
+++ streamlit_demo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.write("Hello Streamlit from GitHub Codespace!")
+
+#streamlit demo
+st.write("Hello Welcome to Creditpath AI Internship")   
+
+st.title("This is the app title")              # Big Title text
+st.header("This is the header")                # Bold Header
+st.markdown("This is the markdown")            # Markdown style text
+st.subheader("This is the subheader")          # Subheader under header
+st.caption("This is the caption")              # Small faded grey text
+
+st.code("x = 2021")
+
+# Mathematical formula
+st.latex(r"""x^a + a^a r^1a r^2a r^3a""") 
